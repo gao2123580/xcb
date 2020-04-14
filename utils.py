b0f82bb089cb48bfb2252339b346387e581761c3
gaokun <1025153924@qq.com>
2020-04-14 14:23:50 +0800

    "元素统一文件"

--- utils.py
+++ utils.py
@@ -8,8 +8,8 @@ def get_driver():
         "platformName": "Android",  # 设备类型(Android / iOS)
         "platformVersion": "5.1.1",  # 系统版本
         "deviceName": "模拟器",  # 设备名称
-        "appPackage": "com.android.settings",  # 待测应用的包名
-        "appActivity": ".Settings",  # 待测应用的启动名
+        "appPackage": "com.bjcsxq.chat.carfriend",  # 待测应用的包名
+        "appActivity": ".MainActivity",  # 待测应用的启动名
         # 解决中文无法输入问题
         "resetKeyboard": True,
         "unicodeKeyboard": True
